3008760c5d831250c86f6f14cb445a00bb3ef3b6
Ryan Tsai <bp4m4h94@gmail.com>
2023-01-20 19:20:37 +0800

    feat: parse player1 successfully

--- PokerHands/Parser.cs
+++ PokerHands/Parser.cs
@@ -2,27 +2,33 @@ namespace PokerHands;
 
 public class Parser
 {
-    public List<Player>  Parse(string input)
+    public List<Player> Parse(string input)
     {
         // Black: 2H 3D 5S 8C 6D  White: 2C 3H 4S 9C 5H
         var playerSection = input.Split("  ", StringSplitOptions.RemoveEmptyEntries);
         var player1Name = playerSection[0].Split(":", StringSplitOptions.RemoveEmptyEntries)[0];
+
+
+        var card1 = playerSection[0]
+            .Split(":")[1]
+            .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+            .Select(x => new Card()
+            {
+                Suit = x[1].ToString(),
+                Output = x[0].ToString(),
+                Value = (int)char.GetNumericValue(x[0])
+            }).ToList();
+        
         var player2Name = playerSection[1].Split(":", StringSplitOptions.RemoveEmptyEntries)[0];
-        return new List<Player>
+        var players = new List<Player>
         {
             new Player() 
             {
                 Name = player1Name, 
-                Card = new List<Card>() 
-                {
-                    new Card(){Suit="H", Value=2, Output="2"}, 
-                    new Card(){Suit="D", Value=3, Output="3"},  
-                    new Card(){Suit="S", Value=5, Output="5"},  
-                    new Card(){Suit="C", Value=8, Output="8"},  
-                    new Card(){Suit="D", Value=6, Output="6"}, 
-                }
+                Card = card1
             },
             new Player() {Name = player2Name},
         };
+        return players;
     }
 }
--- PokerHands/ParserTest.cs
+++ PokerHands/ParserTest.cs
@@ -7,7 +7,7 @@ namespace PokerHands;
 public class ParserTest
 {
     [Test]
-    public void parse_player_name()
+    public void parse_player()
     {
         var parser = new Parser();
         var players = parser.Parse("Black: 2H 3D 5S 8C 6D  White: 2C 3H 4S 9C 5H");
@@ -19,7 +19,7 @@ public class ParserTest
                 Card = new List<Card>() 
                 {
                     new Card(){Suit="H", Value=2, Output="2"}, 
-                    new Card(){Suit="D", Value=3, Output="3"},  
+                    new Card(){Suit="D", Value=3, Output="3"},
                     new Card(){Suit="S", Value=5, Output="5"},  
                     new Card(){Suit="C", Value=8, Output="8"},  
                     new Card(){Suit="D", Value=6, Output="6"}, 
