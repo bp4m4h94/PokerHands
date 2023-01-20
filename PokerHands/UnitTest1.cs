8235f839baff61555395b70b0ffdc7dc75ffc9f6
Ryan Tsai <bp4m4h94@gmail.com>
2023-01-20 11:15:25 +0800

    feat: HighCard vs HighCard mock output

--- PokerHands/UnitTest1.cs
+++ PokerHands/UnitTest1.cs
@@ -1,6 +1,9 @@
+using FluentAssertions;
+
 namespace PokerHands;
 
-public class Tests
+[TestFixture]
+public class GameTests
 {
     [SetUp]
     public void Setup()
@@ -8,8 +11,26 @@ public class Tests
     }
 
     [Test]
-    public void Test1()
+    public void show_winner_card_output()
     {
-        Assert.Pass();
+        var game = new Game();
+
+        // 2~9
+        // Black: 2H 3D 5S 8C 6D  White: 2C 3H 4S 9C 5H
+        //     White wins. - with high card: 9
+        var showResult = game.ShowResult("Black: 2H 3D 5S 8C 6D  White: 2C 3H 4S 9C 5H");
+        showResult.Should().Be("White wins. - with high card: 9");
+
+    }
+}
+
+public class Game
+{
+    public string ShowResult(string input)
+    {
+        var winnerPlayerName = "White";
+        var winnerOutput = "9";
+
+        return $"{winnerPlayerName} wins. - with high card: {winnerOutput}";
     }
 }
