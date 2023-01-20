e6806efcb719179abf05b9b9f6ddfdd00055062b
Ryan Tsai <bp4m4h94@gmail.com>
2023-01-20 15:25:07 +0800

    test: fake player 1

--- PokerHands/Parser.cs
+++ PokerHands/Parser.cs
@@ -10,7 +10,18 @@ public class Parser
         var player2Name = playerSection[1].Split(":", StringSplitOptions.RemoveEmptyEntries)[0];
         return new List<Player>
         {
-            new Player() {Name = player1Name},
+            new Player() 
+            {
+                Name = player1Name, 
+                Card = new List<Card>() 
+                {
+                    new Card(){Suit="H", Value=2, Output="2"}, 
+                    new Card(){Suit="D", Value=3, Output="3"},  
+                    new Card(){Suit="S", Value=5, Output="5"},  
+                    new Card(){Suit="C", Value=8, Output="8"},  
+                    new Card(){Suit="D", Value=6, Output="6"}, 
+                }
+            },
             new Player() {Name = player2Name},
         };
     }
