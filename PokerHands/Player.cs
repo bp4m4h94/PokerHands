95a7ba3b0575cb1a77470dd157b27ea1c26e26fd
Ryan Tsai <bp4m4h94@gmail.com>
2023-01-20 15:21:40 +0800

    test: fail test card

--- PokerHands/Player.cs
+++ PokerHands/Player.cs
@@ -3,4 +3,12 @@ namespace PokerHands;
 public class Player
 {
     public string Name { get; set; }
+    public List<Card> Card { get; set; }
+}
+
+public class Card
+{
+    public string Suit { get; set; }
+    public int Value { get; set; }
+    public string Output { get; set; }
 }
