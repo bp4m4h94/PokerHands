7c06b55074a96269a9e2563afc75d17120cadf67
Ryan Tsai <bp4m4h94@gmail.com>
2023-01-20 14:20:12 +0800

    [fake] hard code return player task case

--- PokerHands/Parser.cs
+++ PokerHands/Parser.cs
@@ -2,9 +2,12 @@ namespace PokerHands;
 
 public class Parser
 {
-    public List<Player> Parse(string input)
+    public List<Player>  Parse(string input)
     {
-        throw new NotImplementedException();
+        return new List<Player>
+        {
+            new Player() { Name = "white" }, new Player() { Name = "black" }
+        };
     }
 }
 
