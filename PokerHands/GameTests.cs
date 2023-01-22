fdc00212726901d6e14fee864eb104e058dc7029
Ryan Tsai <bp4m4h94@gmail.com>
2023-01-22 19:33:11 +0800

    refactor: adjust failed test: show_winner_card_output

--- PokerHands/GameTests.cs
+++ PokerHands/GameTests.cs
@@ -5,21 +5,29 @@ namespace PokerHands;
 [TestFixture]
 public class GameTests
 {
+    private Game _game;
+
     [SetUp]
     public void Setup()
     {
+        _game = new Game();
     }
 
     [Test]
     public void show_winner_card_output()
     {
-        var game = new Game();
-
         // 2~9
         // Black: 2H 3D 5S 8C 6D  White: 2C 3H 4S 9C 5H
         //     White wins. - with high card: 9
-        var showResult = game.ShowResult("Black: 2H 3D 5S 8C 6D  White: 2C 3H 4S 9C 5H");
-        showResult.Should().Be("White wins. - with high card: 9");
+        ResultShouldBe("Black: 2H 3D 5S 8C 6D  White: 2C 3H 4S 9C 5H",
+            "White wins. - with high card: 9");
+        ResultShouldBe("Black: 2H 3D 5S 8C 6D  White: 2C 3H 4S 9C 5H",
+            "Black wins. - with high card: King");
+    }
 
+    private void ResultShouldBe(string input, string expected)
+    {
+        var showResult = _game.ShowResult(input);
+        showResult.Should().Be(expected);
     }
 }
