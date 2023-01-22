771aca57cc3e6e3d502537b1edba853311757d15
Ryan Tsai <bp4m4h94@gmail.com>
2023-01-22 18:04:56 +0800

    refactor to logic: use parser to test player2

--- PokerHands/Game.cs
+++ PokerHands/Game.cs
@@ -4,8 +4,14 @@ public class Game
 {
     public string ShowResult(string input)
     {
-        var winnerPlayerName = "White";
-        var winnerOutput = "9";
+        // Black: 2H 3D 5S 8C 6D  White: 2C 3H 4S 9C 5H
+        var parser = new Parser();
+        var winnerPlayerName = parser.Parse(input)[1].Name;
+        var winnerOutput =  parser
+            .Parse(input)[1]
+            .Card.OrderByDescending(x => x.Value)
+            .First()
+            .Output;
 
         return $"{winnerPlayerName} wins. - with high card: {winnerOutput}";
     }
